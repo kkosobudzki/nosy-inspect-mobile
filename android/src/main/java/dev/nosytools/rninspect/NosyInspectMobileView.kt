4b52063a5701e2ce79ebdd8cff192d1c392f6755
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-10-14 19:39:42 +0200

    chore: initial commit

--- android/src/main/java/dev/nosytools/rninspect/NosyInspectMobileView.kt
+++
@@ -1,7 +0,0 @@
-package dev.nosytools.rninspect
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class NosyInspectMobileView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- android/src/main/java/dev/nosytools/rninspect/NosyInspectModule.kt
+++ android/src/main/java/dev/nosytools/rninspect/NosyInspectModule.kt
@@ -3,7 +3,7 @@ package dev.nosytools.rninspect
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
-class NosyInspectMobileModule : Module() {
+class NosyInspectModule : Module() {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -11,7 +11,7 @@ class NosyInspectMobileModule : Module() {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('NosyInspectMobile')` in JavaScript.
-    Name("NosyInspectMobile")
+    Name("NosyInspect")
 
     // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
     Constants(
@@ -33,15 +33,6 @@ class NosyInspectMobileModule : Module() {
       sendEvent("onChange", mapOf(
         "value" to value
       ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(NosyInspectMobileView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: NosyInspectMobileView, prop: String ->
-        println(prop)
-      }
     }
   }
 }
